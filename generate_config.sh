d61ca018451ed009f9a75d46a99a56f23fd34421
Kevin Darmon <kdarmondev@gmail.com>
2024-10-07 15:34:06 +0200

    Update generate_config.sh

--- generate_config.sh
+++ generate_config.sh
@@ -49,6 +49,10 @@ update_section "AutoAssignCategory" "rules" "${AUTO_ASSIGN_CATEGORY_RULES:-}"
 update_section "AutoAssignTags" "enabled" "${AUTO_ASSIGN_TAGS_ENABLED:-false}"
 update_section "AutoAssignTags" "rules" "${AUTO_ASSIGN_TAGS_RULES:-}"
 
+# AutoAssignAccount
+update_section "AutoAssignAccount" "enabled" "${AUTO_ASSIGN_ACCOUNT_ENABLED:-false}"
+update_section "AutoAssignAccount" "rules" "${AUTO_ASSIGN_ACCOUNT_RULES:-}"
+
 # Logging
 update_section "Logging" "level" "${LOGGING_LEVEL:-INFO}"
 update_section "Logging" "file" "${LOGGING_FILE:-/app/importer.log}"
