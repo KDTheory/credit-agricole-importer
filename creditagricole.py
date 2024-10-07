964490bea32a23258c058265e8801ea5fce52c50
Kevin Darmon <kdarmondev@gmail.com>
2024-10-07 16:42:52 +0200

    Update creditagricole.py

--- creditagricole.py
+++ creditagricole.py
@@ -36,6 +36,8 @@ class CreditAgricoleClient:
 
     def validate(self):
         print("Debug: Entering validate method")
+        
+        # Validation du département
         department = self.config.get('CreditAgricole', 'department', fallback=None)
         print(f"Debug: Raw department value: '{department}'")
         
@@ -44,23 +46,30 @@ class CreditAgricoleClient:
         
         print(f"Debug: Stripped department value: '{department}'")
         
-        if not department or department not in DEPARTMENT:
-            self.logger.error("Please set your bank account department.")
-            raise Exception("Please set your bank account department.")
+        if not department or department not in DEPARTMENTS_TO_CA_REGIONS:
+            self.logger.error("Please set a valid bank account department.")
+            raise Exception("Please set a valid bank account department.")
         
         self.department = department
-        print(f"Debug: Department validation passed with value: '{self.department}'")
+        self.region = DEPARTMENTS_TO_CA_REGIONS[department]
+        print(f"Debug: Department validation passed with value: '{self.department}', Region: '{self.region}'")
     
+        # Validation du nom d'utilisateur
         if not self.config.get('CreditAgricole', 'username'):
             self.logger.error("Please set your bank account username.")
             raise Exception("Please set your bank account username.")
     
+        # Validation du mot de passe
         if not self.config.get('CreditAgricole', 'password'):
             self.logger.error("Please set your bank account password.")
             raise Exception("Please set your bank account password.")
     
+        # Assignation du nom d'utilisateur et du mot de passe
         self.username = self.config.get('CreditAgricole', 'username')
         self.password = self.config.get('CreditAgricole', 'password')
+    
+        print("Debug: Validation completed successfully")
+
 
 
     def init_session(self):
