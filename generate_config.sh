5c1bd9832c29e45e85679ab9f044638bb2500560
Kevin Darmon <kdarmondev@gmail.com>
2024-10-07 15:31:11 +0200

    Update generate_config.sh

--- generate_config.sh
+++ generate_config.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Définir le chemin du fichier de configuration
 CONFIG_FILE="/app/config.ini"
 
-# Fonction pour ajouter ou mettre à jour une section
 update_section() {
     local section=$1
     local key=$2
@@ -21,28 +19,44 @@ update_section() {
     fi
 }
 
-# Créer ou vider le fichier de configuration
 > "$CONFIG_FILE"
 
-# Section GlobalSettings
+# GlobalSettings
 update_section "GlobalSettings" "debug" "${DEBUG:-false}"
 update_section "GlobalSettings" "dry_run" "${DRY_RUN:-false}"
 
-# Section FireflyIII
+# FireflyIII
 update_section "FireflyIII" "url" "${FIREFLY_III_URL}"
 update_section "FireflyIII" "access_token" "${FIREFLY_III_ACCESS_TOKEN}"
 
-# Section CreditAgricole
+# CreditAgricole
 update_section "CreditAgricole" "username" "${CREDIT_AGRICOLE_USERNAME}"
 update_section "CreditAgricole" "password" "${CREDIT_AGRICOLE_PASSWORD}"
 
-# Ajouter la section AutoRenameTransaction
+# AutoRenameTransaction
 update_section "AutoRenameTransaction" "enabled" "${AUTO_RENAME_ENABLED:-false}"
 update_section "AutoRenameTransaction" "rules" "${AUTO_RENAME_RULES:-}"
 
-# Afficher le contenu du fichier de configuration (pour le débogage)
+# AutoAssignBudget
+update_section "AutoAssignBudget" "enabled" "${AUTO_ASSIGN_BUDGET_ENABLED:-false}"
+update_section "AutoAssignBudget" "rules" "${AUTO_ASSIGN_BUDGET_RULES:-}"
+
+# AutoAssignCategory
+update_section "AutoAssignCategory" "enabled" "${AUTO_ASSIGN_CATEGORY_ENABLED:-false}"
+update_section "AutoAssignCategory" "rules" "${AUTO_ASSIGN_CATEGORY_RULES:-}"
+
+# AutoAssignTags
+update_section "AutoAssignTags" "enabled" "${AUTO_ASSIGN_TAGS_ENABLED:-false}"
+update_section "AutoAssignTags" "rules" "${AUTO_ASSIGN_TAGS_RULES:-}"
+
+# Logging
+update_section "Logging" "level" "${LOGGING_LEVEL:-INFO}"
+update_section "Logging" "file" "${LOGGING_FILE:-/app/importer.log}"
+
+# Accounts
+update_section "Accounts" "mapping" "${ACCOUNTS_MAPPING:-}"
+
 echo "Contenu du fichier de configuration :"
 cat "$CONFIG_FILE"
 
-# Exécuter la commande principale
 exec "$@"
