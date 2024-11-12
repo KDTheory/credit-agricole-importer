5099f36930a984c7713c09365fb7c5a20551e672
KDTheory <kdarmondev@gmail.com>
2024-11-12 17:23:35 +0100

    fix missing parenthesis

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ def main():
                     
                     # Vérification des doublons avant l'importation
                     if transaction_key in existing_set:
-                        masked_transaction_key = tuple(mask_sensitive_info(str(item)) for item in transaction_key
+                        masked_transaction_key = tuple(mask_sensitive_info(str(item)) for item in transaction_key)
                         logger.info(f"Doublon détecté pour la transaction : {masked_transaction_key}. Ignorée.")
                         continue
 
