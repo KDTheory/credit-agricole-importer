1febd84f22482d3f951716e6816af414de187893
KDTheory <kdarmondev@gmail.com>
2024-11-17 14:39:43 +0100

    fixing cron not finding python

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,8 @@ echo "Running initial script"
 run_script
 
 # Set up cron job to run at 8 AM every day and log output to Docker logs
-echo "0 8 * * * /bin/bash -c 'python3 /app/main.py' >> /proc/1/fd/1 2>&1" | crontab -
+PYTHON_PATH=$(which python3)
+echo "0 8 * * * /bin/bash -c '$PYTHON_PATH /app/main.py' >> /proc/1/fd/1 2>&1" | crontab -
 
 # Confirm that cron has been set up correctly
 echo "Cron job setup:"
